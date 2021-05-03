e7cc587619a5fdbff5ffaa92ae169de074d6c0f3
Francesco Murdaca <fmurdaca@redhat.com>
2021-05-03 10:49:40 +0200

    Add main file
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

---
+++ app.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+#
+# This script is run by OpenShift's s2i. Here we guarantee that we run desired
+# command
+#
+
+set -o nounset
+set -o errexit
+set -o errtrace
+set -o pipefail
+trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+
+THOTH_PIPELINE_TASK=${THOTH_PIPELINE_TASK:?'THOTH_PIPELINE_TASK is not selected!'}
+
+if [ "$THOTH_WORKFLOW_TASK" = "gather_metrics" ]; then
+    exec python3 gather_metrics.py
+fi
